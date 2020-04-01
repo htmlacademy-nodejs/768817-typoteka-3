2b00c555195fa5a623ad78ccd53c21912966086a
dmitriev_sergey <info@davra.cz>
2020-04-01 22:41:11 +0200

    module3-task2

--- src/express/index.js
+++ src/express/index.js
@@ -4,13 +4,19 @@ const express = require(`express`);
 const myRouter = require(`./routes/my`);
 const commonRouter = require(`./routes/common`);
 const articlesRouter = require(`./routes/articles`);
+const path = require(`path`);
 
+const PUBLIC_DIR = `public`;
 const EXPRESS_DEFAULT_PORT = 8080;
 
 const app = express();
 
 app.use(`/my`, myRouter);
-app.use(`/`, commonRouter);
+app.use(``, commonRouter);
 app.use(`/articles`, articlesRouter);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(EXPRESS_DEFAULT_PORT);
