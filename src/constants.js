2b8aec5953543bb488fd9a77c7841a8548d5583f
dmitriev_sergey <info@davra.cz>
2020-05-05 22:18:01 +0200

    module4-task1

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.FILE_NAME_MOCKS = `mocks.json`;
+module.exports.HttpCodes = {
+  OK: 200,
+  NOT_FOUND: 404,
+  BAD_REQUEST: 400,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports.ID_LENGTH = 6;
+
+module.exports.FILE_SETENCES_PATH = `./src/data/sentences.txt`;
+module.exports.FILE_TITLES_PATH = `./src/data/titles.txt`;
+module.exports.FILE_CATEGORIES_PATH = `./src/data/categories.txt`;
+module.exports.FILE_COMMENTS_PATH = `./src/data/categories.txt`;
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,25 +2,54 @@
 
 const express = require(`express`);
 const fs = require(`fs`).promises;
+const {keys, includes} = require(`ramda`);
 
-const FILE_NAME = `mocks.json`;
+const {FILE_NAME_MOCKS, FILE_CATEGORIES_PATH, HttpCodes} = require(`../../constants`);
+const {readContent, getMocks} = require(`../../utils`);
+const articlesRouter = require(`./routes/articles`);
 const DEFAULT_PORT = 3000;
-const HttpCodes = {
-  OK: 200,
-  NOT_FOUND: 404,
-  INTERNAL_SERVER_ERROR: 500,
-};
+
+const {OK, BAD_REQUEST} = HttpCodes;
+
 
 const app = express();
 app.use(express.json());
+app.use(`/articles`, articlesRouter);
 
 app.get(`/posts`, async (req, res) => {
   try {
-    const content = await fs.readFile(FILE_NAME);
+    const content = await fs.readFile(FILE_NAME_MOCKS);
     const mocks = JSON.parse(content);
     res.json(mocks);
   } catch (err) {
     res.send([]);
+  }
+});
+
+app.get(`/categories`, async (req, res) => {
+  try {
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    return res.status(OK).json(categories);
+  } catch (err) {
+    return res.status(BAD_REQUEST).json([]);
+  }
+});
+
+app.get(`/search`, async (req, res) => {
+  try {
+    const mocks = await getMocks();
+    const queryParams = req.query;
+    const queryKeys = keys(queryParams);
+
+    let filteredMocks = mocks;
+    for (let i = 0; i < queryKeys.length; i++) {
+      let a = filteredMocks;
+      let currentParam = queryKeys[i];
+      filteredMocks = a.filter((item) => includes(queryParams[currentParam], item[currentParam]));
+    }
+    return res.status(200).json(filteredMocks);
+  } catch (err) {
+    return res.status(400).json(`try again`);
   }
 });
 
